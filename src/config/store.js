ad5ea2065a14438e243e91f0644d7027713a6391
Krzysztof Szachna-Krause <kszk@gmx.us>
2020-02-17 22:04:45 +0100

    API added

--- src/config/store.js
+++ src/config/store.js
@@ -1,8 +1,12 @@
 import { createStore, combineReducers } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import cartReducer from "../features/cart/reducer";
+import productsReducer from "../features/product-listing/reducer";
 
-const rootReducer = combineReducers({ cart: cartReducer });
+const rootReducer = combineReducers({
+  cart: cartReducer,
+  products: productsReducer
+});
 
 const store = createStore(rootReducer, composeWithDevTools());
 
--- src/features/product-listing/index.js
+++ src/features/product-listing/index.js
@@ -1,28 +1,32 @@
 import React from "react";
 import { connect } from "react-redux";
 import ProductListItem from "./product-list-item";
+import axios from "axios";
 
-import fetchApi from "../../modules/fetch-api";
 class ProductListing extends React.Component {
-  componentDidMount() {
-    const { loadProducts } = this.props;
-    fetchApi("GET", "http://localhost:5000/api/products").then(json => {
-      loadProducts(json);
-    });
+  async componentDidMount() {
+    try {
+      const { loadProducts } = this.props;
+      const res = await axios.get(
+        "https://kszk-api.herokuapp.com/api/products"
+      );
+      loadProducts(res.data);
+    } catch (err) {
+      console.log(err);
+    }
   }
 
   render() {
+    const { addToCart, removeFromCart, products, cart } = this.props;
     return (
       <div className="product-listing">
-        {this.props.products.map(product => (
+        {products.map(product => (
           <ProductListItem
             key={product.id}
             product={product}
-            addToCart={this.props.addToCart}
-            removeFromCart={this.props.removeFromCart}
-            cartItem={
-              this.props.cart.filter(cartItem => cartItem.id === product.id)[0]
-            }
+            addToCart={addToCart}
+            removeFromCart={removeFromCart}
+            cartItem={cart.filter(cartItem => cartItem.id === product.id)[0]}
           />
         ))}
       </div>
@@ -31,11 +35,13 @@ class ProductListing extends React.Component {
 }
 
 const mapStateToProps = state => ({
-  cart: state.cart
+  cart: state.cart,
+  products: state.products
 });
 
 const mapDispatchToProps = dispatch => {
   return {
+    loadProducts: products => dispatch({ type: "LOAD", payload: products }),
     addToCart: item => dispatch({ type: "ADD", payload: item }),
     removeFromCart: item => dispatch({ type: "REMOVE", payload: item })
   };
